8d2102b1ce07e6bf0f5ef1feb8e57aff3fa0a65b
DaiyaanHoffmanDev <149784268+DaiyaanHoffmanDev@users.noreply.github.com>
2026-02-04 22:33:21 +0200

    Day 6 is where i learnt if statements
    This is where i learnt how to use if , elif and  else statements

---
+++ Day6ifstatments.py
@@ -0,0 +1,11 @@
+# if statement is useed to do some code only if a condition is true Else do something else
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are old enough to drive a car")
+elif age == 17:
+    print("You will be able to drive a car next year")
+else:
+    print("You are not old enough to drive a car")
+
+    # i already learnt this on my own when i was messing around with code so i already know this but im just following along with the course
